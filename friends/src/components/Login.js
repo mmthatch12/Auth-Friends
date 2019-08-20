00ba525d1baed97906ced84e567eddfbad5c1db3
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-20 14:04:50 -0600

    added handle change function

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 const Login = () => {
     const[user, setUser] = useState({ username: '', password: ''})
 
+    handleChange = e => {
+        setUser({
+            ...user, [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <form>
             <input type='text' placeholder='Username' />
