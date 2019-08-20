5689f7a9f4cc287beb7e96463d8efb0778eb5ca7
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-20 14:47:21 -0600

    still working on Formik friends form

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Form, Field, withFormik } from 'formik'
+import axios from 'axios'
 
 
 const FriendList = () => {
     return (
         <Form>
             <Field type='text' name='name' placeholder='Name' />
-            <Field type='text' name='age' placeholder='Age' />
+            <Field type='number' name='age' placeholder='Age' />
             <Field type='email' name='email' placeholder='Email' />
-            
         </Form>
     )
 }
+
+const FormicFriendList = withFormik({
+    mapPropsToValues({ name, age, email }) {
+        return {
+            name: name || '',
+            age: age || 0,
+            email: email || '',
+        }
+    },
+
+    handleSubmit(value, { resetForm, setStatus }) {
+        axios.post('http://localhost:5000/api/friend')
+    }
+})
 
 export default FriendList
