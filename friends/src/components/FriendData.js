0da10de13d2d1c04e9c225a3e3dfcc006e3876ef
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-20 15:46:02 -0600

    still workin on FriendData.js

--- friends/src/components/FriendData.js
+++ friends/src/components/FriendData.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 const FriendData = () => {
+    const[friendList, setFriendList] = useState([])
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get('http://localhost:5000/api/friend')
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err.response))
+
+    }, [])
+
     return (
-        <h1>Friend Data</h1>
+        <h1>Friend data</h1>
     )
+
+
+
 }
 
 export default FriendData
