c76f7f662c593d7b6a98e299d50eaa952649f07d
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-20 13:53:32 -0600

    working on building out Login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const[user, setUser] = useState({ username: '', password: ''})
+
     return (
         <form>
             <input type='text' placeholder='Username' />
             <input type='text' placeholder='Password' />
+            <button>Submit</button>
         </form>
     )
 }
